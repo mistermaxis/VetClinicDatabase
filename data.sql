98733a334480b1980dba7f7f9c98f3d8bdc935c7
Maximiliano Diaz <87093942+mistermaxis@users.noreply.github.com>
2021-12-14 18:14:09 -0300

    Update data.sql
    Add required animal data

--- data.sql
+++ data.sql
@@ -11,3 +11,23 @@ values ('Pikachu', date '2021-01-07', 1, false, 15.04);
 
 insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 values ('Devimon', date '2017-05-12', 5, true, 11.0);
+
+////////////////////
+
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+values ('Charmander', date '2020-02-08', 0, false, -11.0);
+
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg)                                                                        
+values ('Plantmon', date '2022-11-15', 2, true, -5.7);
+
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+values ('Squirtle', date '1993-04-02', 3, false, -12.13);
+
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+values ('Angemon', date '2005-06-12', 1, true, -45.0);
+
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+values ('Boarmon', date '2005-07-07', 7, true, 20.4);
+
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+values ('Blossom', date '1998-10-13', 3, true, 17.0);
