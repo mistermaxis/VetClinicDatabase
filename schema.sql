70ef9a26f705b8af4b8be8bd86c259f09667bb9a
Maximiliano Diaz <87093942+mistermaxis@users.noreply.github.com>
2021-12-16 11:30:21 -0300

    Update schema.sql
    - Create owners table.
    - Create species table,
    - Modify animals table.

--- schema.sql
+++ schema.sql
@@ -1,12 +1,28 @@
 /* Database schema to keep the structure of entire database. */
 
+CREATE TABLE owners (
+    id serial primary key,
+    full_name varchar(50),
+    age int
+);
+
+CREATE TABLE species (
+    id serial primary key,
+    name varchar(50),
+);
+
 CREATE TABLE animals (
-    id int,
+    id serial primary key,
     name varchar(50),
     date_of_birth date,
     escape_attempts int,
     neutered bool,
     weight_kg decimal,
-    species varchar(50),
+    species_id int,
+    owner_id int,
+    foreign key(species_id)
+        references species(id),
+    foreign key(owner_id)
+        references owners(id),
 );
 
