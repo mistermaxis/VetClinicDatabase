43b488eb6341836ffe4f5edfe830fbee91e2290b
Maximiliano Diaz <87093942+mistermaxis@users.noreply.github.com>
2021-12-17 13:02:59 -0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -42,3 +42,19 @@ alter table animals
 add constraint owners_key
 foreign key(owner_id)
 references owners(id);
+
+/*      Join Table      */
+
+create table vets (
+id serial primary key,
+name varchar(50),
+age int,
+graduation date,
+);
+
+create table specializations (
+vet_id int,
+species_id int,
+constraint vet_id foreign key(vet_id) references vets(id),
+constraint species_id foreign key(species_id) references species(id)
+);
