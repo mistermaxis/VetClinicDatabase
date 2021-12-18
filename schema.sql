31a1e9d3ae16a89f72c92c27c5bada8a88e1e6b2
Maximiliano Diaz <87093942+mistermaxis@users.noreply.github.com>
2021-12-18 10:55:44 -0300

    Update schema.sql
    Remove duplicate visits table

--- schema.sql
+++ schema.sql
@@ -59,14 +59,6 @@ constraint vet_id foreign key(vet_id) references vets(id),
 constraint species_id foreign key(species_id) references species(id)
 );
 
-CREATE TABLE VISITS(
-    ANIMALS_ID INT,
-    CONSTRAINT ANIMALS_ID FOREIGN KEY(ANIMALS_ID) REFERENCES ANIMALS(ID),
-    VETS_ID INT,
-    CONSTRAINT VETS_ID FOREIGN KEY(VETS_ID) REFERENCES VETS(ID),
-    DATE_OF_VISIT DATE
-);
-
 create table visits (
 animal_id int,
 vet_id int,
